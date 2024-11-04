de280587ac0752224e0ec64ac5fe67f39c5751ef
Andrey <63256342+adastion@users.noreply.github.com>
2024-11-05 01:21:56 +0300

    creating adaptive screen artists__list for mobile

--- src/ui/components/artists/Artists.js
+++ src/ui/components/artists/Artists.js
@@ -4,7 +4,7 @@ export function Artists(data) {
   const artists = data
 
   const artistsListElement = document.createElement("ul")
-  artistsListElement.style = "margin: 0; padding: 0; list-style: none; display: grid; grid-template-columns: repeat(2, 1fr); gap: 15px; "
+  artistsListElement.classList.add("artists__list")
 
   artists.map(artist => {
     artistsListElement.append(CardArtist(artist))
